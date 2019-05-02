04b5a12b4251e92a083c9389472c350b292f8a6b
root <entriken@chapman.edu>
2019-05-02 04:32:52 +0000

    delete doesnt work

--- Database.h
+++ Database.h
@@ -2,7 +2,7 @@
 #include "BST.h"
 #include "Student.h"
 #include "Faculty.h"
-//#include "DoublyLinkedList.h"
+#include<limits>
 
 using namespace std;
 
@@ -17,7 +17,6 @@ class Database
       sBST = BST<Student>();
       fBST = BST<Faculty>();
     }
-
 
     //------------------- 1 -------------------
     void printStudents()
@@ -96,18 +95,24 @@ class Database
       cout << "ADD NEW STUDENT" << endl;
       cout << "What is the student's id? ... ";
       cin >> id;
+      cin.clear();
       cout << "What is the student's name? ... "; //typing first name and last name ("Jon Apple") will skip the next cin (grade)
-      cin >> name;
+      cin.ignore();
+      getline(cin, name);
       cout << "What is the student's grade? ... ";
       getline(cin, grade);
       cout << "What is the student's major? ... ";
-      cin >> major;
+      getline(cin, major);
       cout << "What is the student's gpa? ... ";
       cin >> gpa;
+      cin.clear();
       cout << "What is the id number of the student's advisor? ... ";
       cin >> advisor;
 
       sBST.insert(Student(id, name, grade, major, gpa, advisor));
+      Faculty x = (Faculty(advisor, "irrelevant", "irrelevant", "irrelevant", DoublyLinkedList()));
+      fBST.retrieve(x).advisees.insertFront(id);
+
       cout << "Student has been successfully added" << endl;
     }
 
@@ -115,7 +120,14 @@ class Database
     void deleteStudent(int id)
     {
       Student x = Student(id, "irrelevant", "irrelevant", "irrelevant", 0.0, 0000000);
-      sBST.remove(x);
+      if(sBST.contains(x))
+      {
+        sBST.remove(Student(id, "irrelevant", "irrelevant", "irrelevant", 0.0, 0000000));
+      }
+      else
+      {
+        cout << " Student " << id << " is not in our database." << endl;
+      }
     }
 
     //------------------- 9 -------------------
@@ -132,13 +144,14 @@ class Database
       cout << "What is the faculty's id? ... ";
       cin >> id;
       cout << "What is the faculty's name? ... ";
-      cin >> name;
+      cin.ignore();
+      getline(cin, name);
       cout << "What is the faculty's level? ... ";
-      cin >> level;
+      getline(cin, level);
       cout << "What is the faculty's department? ... ";
-      cin >> department;
-      cout << "Enter the id's of this advisor's advisees one at a time" << endl;
-      cout << "If you are finished entering advisees, enter any letter to finish ... ";
+      getline(cin, department);
+      cout << "If you are finished entering advisees, enter any letter to finish" << endl;
+      cout << "Enter the id's of this advisor's advisees one at a time ... ";
       while(cin >> advisee)
       {
         advisees.insertFront(advisee);
@@ -146,39 +159,208 @@ class Database
       }
       fBST.insert(Faculty(id, name, level, department, advisees));
       cout << "Faculty has been successfully added" << endl;
+      cin.ignore();
+      cin.clear();
     }
 
     //------------------- 10 -------------------
     void deleteFaculty(int id)
     {
       Faculty x = (Faculty(id, "irrelevant", "irrelevant", "irrelevant", DoublyLinkedList()));
-      fBST.remove(x);
+      if(fBST.contains(x))
+      {
+        fBST.remove(x);
+      }
+      else
+      {
+        cout << " Faculty " << id << " is not in our database." << endl;
+      }
     }
-
 
     //------------------- 11 -------------------
     void changeAdvisor(int student_id, int faculty_id)
     {
-
+      Student a = Student(student_id, "irrelevant", "irrelevant", "irrelevant", 0.0, 0000000);
+      if(sBST.contains(a))
+      {
+        Student x = Student(sBST.retrieve(a).id, sBST.retrieve(a).name, sBST.retrieve(a).grade, sBST.retrieve(a).major, sBST.retrieve(a).gpa, faculty_id);
+        sBST.remove(a);
+        sBST.insert(x);
+      }
+      else
+      {
+        cout << " Student " << student_id << " is not in our database." << endl;
+      }
     }
 
     //------------------- 12 -------------------
     void removeAdvisee(int student_id, int faculty_id)
     {
+      Student a = Student(student_id, "irrelevant", "irrelevant", "irrelevant", 0.0, 0000000);
+      Faculty b = Faculty(faculty_id, "irrelevant", "irrelevant", "irrelevant", DoublyLinkedList());
+      DoublyLinkedList advisees;
 
+      if(sBST.contains(a) && fBST.contains(b))
+      {
+        advisees = fBST.retrieve(b).advisees;
+        advisees.deleteNode(student_id);
+        Faculty x = Faculty(fBST.retrieve(b).id, fBST.retrieve(b).name, fBST.retrieve(b).level, fBST.retrieve(b).department, advisees);
+        fBST.remove(b);
+        fBST.insert(x);
+      }
+      else if(sBST.contains(a))
+      {
+        cout << " Student " << student_id << " is not in our database." << endl;
+      }
+      else if(fBST.contains(b))
+      {
+        cout << " Faculty " << faculty_id << " is not in our database." << endl;
+      }
+      else
+      {
+        cout << " Student " << student_id << " is not in our database." << endl;
+        cout << " Faculty " << faculty_id << " is not in our database." << endl;
+      }
     }
-
     //------------------- 13 -------------------
     void rollback()
     {
+      cout << "not implemented" << endl;
 
+      /*
+      Create a list that is written to whenever a change happens
+      Functions that can be rolled back: 7-12
+      Whenever these functions run, they insert to a stack
+      Pop stack and see what the last change was to revert
+      */
     }
 
     //------------------- 14 -------------------
     void exitProgram()
     {
+
+      //write to file
+
       cout << "Exiting Program" << endl;
       exit(0);
     }
 
+    //------------------------------------------
+    void Process()
+    {
+
+      //read from file and create BST
+
+      int response;
+      int id;
+      while(true)
+      {
+        response = 0;
+        cout << "\n------------------------- STUDENT AND FACULTY DATABASE -------------------------" << endl;
+        cout << "1. Print all students and their information (sorted by ascending id #)"             << endl;
+        cout << "2. Print all faculty and their information (sorted by ascending id #)"              << endl;
+        cout << "3. Find and display student information given the students id"                      << endl;
+        cout << "4. Find and display faculty information given the faculty id"                       << endl;
+        cout << "5. Given a student’s id, print the name and info of their faculty advisor"          << endl;
+        cout << "6. Given a faculty id, print ALL the names and info of his/her advisees"            << endl;
+        cout << "7. Add a new student"                                                               << endl;
+        cout << "8. Delete a student given the id"                                                   << endl;
+        cout << "9. Add a new faculty member"                                                        << endl;
+        cout << "10. Delete a faculty member given the id"                                           << endl;
+        cout << "11. Change a student’s advisor given the student id and the new faculty id"         << endl;
+        cout << "12. Remove an advisee from a faculty member given the ids"                          << endl;
+        cout << "13. Rollback"                                                                       << endl;
+        cout << "14. Exit"                                                                           << endl;
+        cout << "----------------------------------------------------------------------------------" << endl;
+        cout << " ... ";
+        cin >> response;
+
+        if(response == 1)
+        {
+          printStudents();
+          cin.clear();
+        }
+        if(response == 2)
+        {
+          printFaculty();
+        }
+        if(response == 3)
+        {
+          cout << "Enter the id of the student you would like to print ... ";
+          cin >> id;
+          displayStudent(id);
+        }
+        if(response == 4)
+        {
+          cout << "Enter the id of the faculty you would like to print ... ";
+          cin >> id;
+          displayFaculty(id);
+        }
+        if(response == 5)
+        {
+          cout << "Enter the id of the student whose advisor you would like to print ... ";
+          cin >> id;
+          displayAdvisor(id);
+        }
+        if(response == 6)
+        {
+          cout << "Enter the id of the faculty whose advisees you would like to print ... ";
+          cin >> id;
+          displayAdvisees(id);
+        }
+        if(response == 7)
+        {
+          addStudent();
+        }
+        if(response == 8)
+        {
+          cout << "Enter the id of the student who you would like to delete ... ";
+          cin >> id;
+          deleteStudent(id);
+        }
+        if(response == 9)
+        {
+          addFaculty();
+        }
+        if(response == 10)
+        {
+          cout << "Enter the id of the faculty who you would like to delete ... ";
+          cin >> id;
+          deleteFaculty(id);
+        }
+        if(response == 11)
+        {
+          int sid, fid;
+          cout << "Enter the id of the student whose advisor you would like to change ... ";
+          cin >> sid;
+          cout << "Enter the id of the advisor ... ";
+          cin >> fid;
+          changeAdvisor(sid, fid);
+        }
+        if(response == 12)
+        {
+          int sid, fid;
+          cout << "Enter the id of the faculty whose advisee you would like to remove ... ";
+          cin >> fid;
+          cout << "Enter the id of the advisee (student) ... ";
+          cin >> sid;
+          removeAdvisee(sid, fid);
+        }
+        if(response == 13)
+        {
+          cout << "Not yet implemented" << endl;
+        }
+        if(response == 14)
+        {
+          exitProgram();
+          return;
+        }
+        else
+        {
+          cin.clear();
+          cin.ignore(numeric_limits<streamsize>::max(), '\n'); //https://stackoverflow.com/questions/10828937/how-to-make-cin-take-only-numbers
+          cout << "Please enter a valid number" << endl;
+        }
+      }
+    }
 };
--- main.cpp
+++ main.cpp
@@ -5,103 +5,16 @@ using namespace std;
 
 main()
 {
-  /*
-  BST<int> bst;
-  bst.insert(10);
-  bst.insert(98);
-  bst.insert(8);
-  bst.insert(7);
-  bst.insert(1);
-  bst.insert(30);
-  bst.insert(32);
-  bst.insert(100);
-  bst.recPrint(bst.root);
-
-  cout << "REMOVE 8" << endl;
-  bst.remove(8);
-  bst.recPrint(bst.root);
-
-  cout << "REMOVE 98" << endl;
-  bst.remove(98);
-  bst.recPrint(bst.root);
-
-  cout << "REMOVE 30" << endl;
-  bst.remove(30);
-  bst.recPrint(bst.root);
-  */
+  Database d;
 /*
-  BST<Student> bst;
-  bst.insert(Student(2297510, "Student A", "Freshman", "Business", 2.9, 0000005));
-  bst.insert(Student(2297598, "Student B", "Freshman", "English", 3.6, 0000005));
-  bst.insert(Student(2297508, "Student C", "Freshman", "Undecided", 2.3, 0000006));
-  bst.insert(Student(2297507, "Student D", "Sophmore", "Comp Sci", 3.4, 0000006));
-  bst.insert(Student(2297501, "Student E", "Sophmore", "Business", 3.1, 0000007));
-  bst.insert(Student(2297530, "Student F", "Junior", "Political Sci", 3.5, 0000007));
-  bst.insert(Student(2297532, "Student G", "Junior", "Physics", 3.7, 0000010));
-  bst.insert(Student(2297600, "Student H", "Senior", "Chemistry", 3.1, 0000011));
-  bst.recPrint(bst.root);
-
-  cout << "REMOVE Student ID 2297508" << endl;
-  bst.remove(Student(2297508, "irrelevant", "irrelevant", "irrelevant", 0.0, 0000000));
-  bst.recPrint(bst.root);
-
-  cout << "REMOVE Student ID 2297598" << endl;
-  bst.remove(Student(2297598, "irrelevant", "irrelevant", "irrelevant", 0.0, 0000000));
-  bst.recPrint(bst.root);
-
-  cout << "REMOVE Student ID 2297530" << endl;
-  bst.remove(Student(2297530, "irrelevant", "irrelevant", "irrelevant", 0.0, 0000000));
-  bst.recPrint(bst.root);
-  */
-
-  BST<Faculty> bst;
-  bst.insert(Faculty(2297356, "Faculty A", "Professor", "Science Department", DoublyLinkedList()));
-  bst.insert(Faculty(2297832, "Faculty B", "Professor", "Business School", DoublyLinkedList()));
-  bst.insert(Faculty(2297111, "Faculty C", "Professor", "Medical School", DoublyLinkedList()));
-  bst.insert(Faculty(2297990, "Faculty D", "Professor", "Law School", DoublyLinkedList()));
-  bst.insert(Faculty(2297586, "Faculty E", "Professor", "Art School", DoublyLinkedList()));
-  bst.insert(Faculty(2297293, "Faculty F", "Professor", "Education Department", DoublyLinkedList()));
-  bst.insert(Faculty(2297444, "Faculty G", "Department Head", "Science Department", DoublyLinkedList()));
-  bst.insert(Faculty(2297600, "Faculty H", "Department Head", "Business School", DoublyLinkedList()));
-  bst.recPrint(bst.root);
-
-  cout << "REMOVE Faculty ID 2297832" << endl;
-  bst.remove(Faculty(2297832, "irrelevant", "irrelevant", "irrelevant", DoublyLinkedList()));
-  bst.recPrint(bst.root);
-
-  cout << "REMOVE Faculty ID 2297990" << endl;
-  bst.remove(Faculty(2297990, "irrelevant", "irrelevant", "irrelevant", DoublyLinkedList()));
-  bst.recPrint(bst.root);
-
-  cout << "REMOVE Faculty ID 2297356" << endl;
-  bst.remove(Faculty(2297356, "irrelevant", "irrelevant", "irrelevant", DoublyLinkedList()));
-  bst.recPrint(bst.root);
-
-  cout << "testing insertion of a faculty member with > 0 advisees (Faculty X)" << endl;
-  Faculty a = Faculty(2297356, "Faculty X", "Professor", "Science Department", DoublyLinkedList());
-  a.advisees.insertFront(2297574);
-  bst.insert(a);
-  bst.recPrint(bst.root);
-
-  cout << "SEARCH" << endl;
-  cout << bst.contains(Faculty(2297356, "irrelevant", "irrelevant", "irrelevant", DoublyLinkedList())) << endl;
-  cout << bst.retrieve(Faculty(2297356, "irrelevant", "irrelevant", "irrelevant", DoublyLinkedList())) << endl;
-
-  cout << endl;
-  cout << endl;
-  cout << "testing the database functions" << endl;
-  cout << endl;
-
-  Database d = Database();
-
-  d.sBST.insert(Student(2297510, "Student A", "Freshman", "Business", 2.9, 2297990));
-  d.sBST.insert(Student(2297598, "Student B", "Freshman", "English", 3.6, 0000005));
-  d.sBST.insert(Student(2297508, "Student C", "Freshman", "Undecided", 2.3, 0000006));
-  d.sBST.insert(Student(2297507, "Student D", "Sophmore", "Comp Sci", 3.4, 0000006));
-  d.sBST.insert(Student(2297501, "Student E", "Sophmore", "Business", 3.1, 0000007));
-  d.sBST.insert(Student(2297530, "Student F", "Junior", "Political Sci", 3.5, 0000007));
-  d.sBST.insert(Student(2297532, "Student G", "Junior", "Physics", 3.7, 0000010));
-  d.sBST.insert(Student(2297600, "Student H", "Senior", "Chemistry", 3.1, 0000011));
+  d.sBST.insert(Student(2297510, "Student A", "Freshman", "Business", 2.9, 2297356));
+  d.sBST.insert(Student(2297598, "Student B", "Freshman", "English", 3.6, 2297832));
+  d.sBST.insert(Student(2297508, "Student C", "Freshman", "Undecided", 2.3, 2297111));
+  d.sBST.insert(Student(2297507, "Student D", "Sophmore", "Comp Sci", 3.4, 2297990));
+  d.sBST.insert(Student(2297501, "Student E", "Sophmore", "Business", 3.1, 2297586));
+  d.sBST.insert(Student(2297530, "Student F", "Junior", "Political Sci", 3.5, 2297293));
+  d.sBST.insert(Student(2297532, "Student G", "Junior", "Physics", 3.7, 2297444));
+  d.sBST.insert(Student(2297600, "Student H", "Senior", "Chemistry", 3.1, 2297600));
 
   d.fBST.insert(Faculty(2297356, "Faculty A", "Professor", "Science Department", DoublyLinkedList()));
   d.fBST.insert(Faculty(2297832, "Faculty B", "Professor", "Business School", DoublyLinkedList()));
@@ -111,27 +24,6 @@ main()
   d.fBST.insert(Faculty(2297293, "Faculty F", "Professor", "Education Department", DoublyLinkedList()));
   d.fBST.insert(Faculty(2297444, "Faculty G", "Department Head", "Science Department", DoublyLinkedList()));
   d.fBST.insert(Faculty(2297600, "Faculty H", "Department Head", "Business School", DoublyLinkedList()));
-
-  d.printStudents();
-  cout << endl;
-  d.printFaculty();
-  cout << endl;
-
-  d.displayStudent(2297530);
-  cout << endl;
-  d.displayFaculty(2297356);
-
-  cout << "5. Display Advisor" << endl;
-  d.displayAdvisor(2297510);
-
-  //cout << "6. Display Advisees" << endl;
-  //d.displayAdvisees(2297356);
-
-  cout << "7. Add Student" << endl;
-  //d.printStudents();
-  //d.addStudent();
-  //d.printStudents();
-  d.printFaculty();
-  d.addFaculty();
-  d.printFaculty();
+*/
+  d.Process();
 }
